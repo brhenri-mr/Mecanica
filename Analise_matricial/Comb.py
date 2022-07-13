444c9579de334ffb5ee81ed39069a861d04f1440
brhenri-mr <83376956+brhenri-mr@users.noreply.github.com>
2022-07-13 18:00:32 -0300

    comm004
    resolvendo problema de mais de um carregamento variavel

--- Analise_matricial/Comb.py
+++ Analise_matricial/Comb.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 #Combinações segundo a nbr8681
 
 fact = {
@@ -19,15 +18,24 @@ fact = {
   
     }}
 
-case = [{'CP':[1.35,1]},{'SC':[1.5,0]},{'W':[1.5,0]}]
+case = [{'CP':[1.35,1]},{'SCesc':[1.5,0]},{'W90':[1.5,0]}]
 
-
-def comb(db):
-  
+def comb(db: list) -> list:
+    """
+    Função que retorna a lista de todas as combinações possíveis para um determinado patter
+    
+    """
     s = []
     r= 0
     permutacao = 2**len(db)
     temp = permutacao/2
+
+    # variaveis para correção para mais de uma carga variavel
+    carr_variaveis = [] #lista dos carregamento variaives com seu nome e coeficiente de ponderação de esforço para esforço secundário
+    varr = 0
+    add = 0
+    temp_dois = []
+    e= 0
   
     # definir os 2 primeiros casos
 
@@ -49,16 +57,78 @@ def comb(db):
                             else:
                                 cri = temp +((r-int(permutacao/2))//temp)*temp
                             vezes = vezes + cri
-                            '''
+                        '''
                             if r>int(permutacao/2):
                                 vezes = vezes + int(permutacao/2)
                                 vezes  = vezes + 
                             else:
                                 vezes = vezes + (r//temp)*temp
-                            '''
+                        
+                        if tipo in carr_variaveis:
+                            if varr == 1:
+
+                            else:
+                                varr = 1
+                            pass
+                        else:
+                        '''
+
                         s[vezes].append([tipo,coef])
                         r += 1
         r = 0     
         temp = int(temp/2)
-    return s
+    
+    #definindo para caso de mais de um combinação variável
+
+    for arranjo in s:
+        for caso in arranjo:
+
+            #contagem de quanto casos de carr variavel na combinação
+            if caso[0] in carr_variaveis:
+                add =+ 1
+            else:
+                pass
+
+        if add > 1:
+            for vezes in range(add-1):
+                fant = arranjo.copy()
+                for caso in fant:
+                     if caso[0] in carr_variaveis:
+                         e =+1
+
+                         if add**vezes == e: #resolver isso aqui: uma relação entre vezes ou add com e para que garanta que sempre oule de número em número
+                            
+                            for tipo in carr_variaveis:
+                                for carr_ in tipo:
+                                    if carr_[0] == caso[0]:
+                                        multi = carr_[1]
+                                        break
+            
+
+                            caso[1] = caso[1]*multi
+
+                         else:
+                            pass
+
+                temp_dois.append(fant.copy())
+
+                         
+
+
+
+                # é preciso clonar o caso e fazer a devida correção
+                
+
+        temp_dois.append(arranjo)
+
+
+
+                
         
+
+
+    return s
+
+
+
+print(comb(case))
